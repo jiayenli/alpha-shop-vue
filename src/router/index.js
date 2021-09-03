4c5c80549da41a4c8e3562c33cf564e809401234
jiayenli <x9276550@gmail.com>
2021-09-04 01:50:34 +0800

    系統設定

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     redirect: '/AlphaShop/Checkout'
   },
+  {
+    path: '/AlphaShop',
+    name: 'AlphaShop',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    redirect: '/AlphaShop/Checkout'
+  },
 
   {
     path: '/AlphaShop/Checkout',
